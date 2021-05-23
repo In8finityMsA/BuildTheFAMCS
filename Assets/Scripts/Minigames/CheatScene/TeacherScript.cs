943332c8a59f1a0ef42321ca8a0408c94180ea0f
TsemushevaAnastasia <temusheva.nastya@gmail.com>
2021-05-24 02:36:09 +0300

    teacher anime fix

--- Assets/Scripts/Minigames/CheatScene/TeacherScript.cs
+++ Assets/Scripts/Minigames/CheatScene/TeacherScript.cs
@@ -147,6 +147,8 @@ public class TeacherScript : MonoBehaviour
     void UIIsTurning()
     {
         animator.SetBool("IsTurning", true);
+        animator.SetBool("IsWatching", false);
+        animator.SetBool("IsNotWatching", false);
         GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 1.0f);
         //TODO
         //place to start animation of turning teacher (if we have, else nothing)
@@ -156,6 +158,8 @@ public class TeacherScript : MonoBehaviour
     void UIIsNotWatching()
     {
         animator.SetBool("IsNotWatching", true);
+        animator.SetBool("IsTurning", false);
+        animator.SetBool("IsWatching", false);
         Debug.Log("Busy!");
         GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
         //TODO
@@ -165,6 +169,8 @@ public class TeacherScript : MonoBehaviour
     void UIIsWatching()
     {
         animator.SetBool("IsWatching", true);
+        animator.SetBool("IsTurning", false);
+        animator.SetBool("IsNotWatching", false);
         Debug.Log("Watching!");
         GetComponent<SpriteRenderer>().color = new Color(1.0f, 0f, 0f);
         //TODO
