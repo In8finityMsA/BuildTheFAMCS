065bf1ca6955a559da5ea1f7925b41ed153b18ef
veta1428 <lizavetka1428@gmail.com>
2021-05-24 15:42:46 +0300

    server for mobile

--- Assets/Scripts/Minigames/ServerScene/TapScript.cs
+++ Assets/Scripts/Minigames/ServerScene/TapScript.cs
@@ -9,29 +9,35 @@ public class TapScript : MonoBehaviour
     public float time;
     public float timeDown;
     public float tapSpeed;
+    // public float newTapSpeed;
     public float newTapSpeed;
     public bool isEnding;
     public const float maxLoad = 100;
     public const float minLoad = 0;
 
-    public float delta;
+    public float deltaDOWN;
+    public float deltaUP;
     public float A;
     public float B;
 
     public float curLoad;
 
     public delegate void OnTapSpeedChanged();
-
+    float acseleration;
     public delegate void OnStart();
     public event OnStart AnimationStartHandler;
 
+    int downCounter;
     public int taps;
     public int times;
+    float timing = 0;
+
 
     // Start is called before the first frame update
 
     void Start()
     {
+        acseleration = 0.5f;
         mouseDown = false;
         timeDown = 0;
         isEnding = false;
@@ -44,7 +50,8 @@ public class TapScript : MonoBehaviour
         //params
         A = 5;
         B = 6;
-        delta = (float)0.05;
+        deltaDOWN = (float)0.03;
+        deltaUP = (float)0.04;
 
         ManagerTapScript.Instance.tap = this;
     }
@@ -58,23 +65,40 @@ public class TapScript : MonoBehaviour
 
     void Update()
     {
-
+        timing += Time.deltaTime;
         time += Time.deltaTime;
-        newTapSpeed = 1 / (time - timeDown);
+
+        
+            if (Input.touchCount > 0)
+            {
+                print("ther is a touch");
+
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                downCounter++;
+                    print("Touch has Began");
+                }
+                if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    print("Touch has Ended");
+                }
+            }
+
+        //newTapSpeed = 1 / (time - timeDown);
 
         if (newTapSpeed >= B)
         {
             if (newTapSpeed - B >= 0.5f * B)
             {
-                curLoad += -(float) delta * 3;
+                curLoad += -(float)deltaDOWN * 3;
             }
             else if (newTapSpeed - B >= 0.33f * B)
             {
-                curLoad += -(float) delta * 2;
+                curLoad += -(float)deltaDOWN * 2;
             }
             else
             {
-                curLoad += -(float) delta;
+                curLoad += -(float)deltaDOWN;
             }
         }
 
@@ -82,15 +106,15 @@ public class TapScript : MonoBehaviour
         {
             if (A - newTapSpeed >= 0.5f * A)
             {
-                curLoad += (float) delta * 3;
+                curLoad += (float)deltaUP * 3;
             }
             else if (A - newTapSpeed >= 0.33f * A)
             {
-                curLoad += (float) delta * 2;
+                curLoad += (float)deltaUP * 2;
             }
             else
             {
-                curLoad += (float) delta;
+                curLoad += (float)deltaUP;
             }
         }
 
@@ -104,13 +128,24 @@ public class TapScript : MonoBehaviour
             curLoad = maxLoad;
         }
     }
-    
+
+    private void FixedUpdate()
+    {
+        if (timing >= acseleration)
+        {
+            timing = 0;
+            newTapSpeed = downCounter / acseleration;
+        }
+        
+    }
+
     void OnMouseDown()
     {
         if (!isEnding)
         {       
             mouseDown = true;
             timeDown = time;
+            Debug.Log("dowwwwwn");
         }
     }
 
